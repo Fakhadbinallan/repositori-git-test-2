abbbc8606e8d7b6dd4939140b8f1fef382ffa9c3
fakhadbinallan <thebalcklist23@gmail.com>
2023-05-15 09:31:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus incidunt pariatur suscipit sapiente ipsa quos fuga beatae? Ad repudiandae eaque corrupti voluptatum at repellendus! Eius similique cupiditate ullam mollitia sed.</p>
+    <ul>
+        <li>meysita</li>
+        <li>fakhad</li>
+    </ul>
 </body>
 </html>
